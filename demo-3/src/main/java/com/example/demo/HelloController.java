3301b4dff956089c7c5c40f1c5f1f5909eee695c
NandhA-KuMar03 <nandhakumar.kumaran@cdw.com>
2023-04-10 14:57:03 +0530

    review comments

--- demo-3/src/main/java/com/example/demo/HelloController.java
+++ demo-3/src/main/java/com/example/demo/HelloController.java
@@ -4,66 +4,69 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public void sayHello() {
 		System.out.println("Hello");
 	}
 	
-	@RequestMapping(value = "hello1" , method = RequestMethod.GET)
+	@RequestMapping(value = "page1" , method = RequestMethod.GET)
 	public String sayHello1() {
-		System.out.println("Hello 1");
+		System.out.println("Page 1");
 		return "Welcome";
 	}
 	
-	@RequestMapping(value = "hello2" , method = RequestMethod.GET)
+	@RequestMapping(value = "page2" , method = RequestMethod.GET)
 	public ModelAndView sayHello2(){
-		ModelAndView mandv = new ModelAndView();
-		mandv.addObject("obj1" , "Welcome to obj1");
-		mandv.setViewName("Welcome1");
-		return mandv;
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("obj1" , "Welcome to obj1");
+		modelAndView.setViewName("Welcome1");
+		return modelAndView;
 	}
 	
-	@RequestMapping(value="hello21" , method = RequestMethod.GET)
+	@RequestMapping(value="page3" , method = RequestMethod.GET)
 	public ResponseEntity<String> sayHello21(){
 		return ResponseEntity.ok("This is a response entity");
 	}
 	
-	@RequestMapping(value = "hello3" , method = RequestMethod.GET)
+	@RequestMapping(value = "page4" , method = RequestMethod.GET)
 	public ModelAndView sayHello3(ModelAndView mandv){
 		mandv.addObject("obj2" , "Welcome to obj2");
 		mandv.setViewName("Welcome2");
 		return mandv;
 	}
 	
-	@RequestMapping(value = "hello4" , method = RequestMethod.GET)
-	public ModelAndView sayHello6(ModelAndView mandv, HttpServletRequest request) {
-		mandv.addObject("obj6" , "Welcome to obj6");
-		mandv.setViewName("Welcome3");
+	@RequestMapping(value = "page5" , method = RequestMethod.GET)
+	public ModelAndView sayHello6(ModelAndView modelAndView, HttpServletRequest request) {
+		modelAndView.addObject("obj3" , "Welcome to obj3");
+		modelAndView.setViewName("Welcome3");
 		HttpSession session = request.getSession();
 		request.setAttribute("requestObj", "welcome to request object");
 		session.setAttribute("sessionObj", "Welcome to session");
-		return mandv;
+		return modelAndView;
 	}
 	
-	@RequestMapping(value = "repeat" , method = RequestMethod.GET)
-	public ModelAndView repeat(ModelAndView mandv, HttpServletRequest request) {
-		mandv.addObject("obj6" , "Welcome to obj6 in session");
-		mandv.setViewName("Welcome3");
+	@RequestMapping(value = "page5Repeat" , method = RequestMethod.GET)
+	public ModelAndView repeat(ModelAndView modelAndView, HttpServletRequest request) {
+		modelAndView.addObject("obj3" , "Welcome to obj3 in session");
+		modelAndView.setViewName("Welcome3");
 		request.setAttribute("requestObj", "welcome to request object in repeat");
-		return mandv;
+		return modelAndView;
 	}
 	
-	@RequestMapping(value = "hello7" , method = RequestMethod.GET)
+	@RequestMapping(value = "page6" , method = RequestMethod.GET)
 	public ResponseEntity<User> sayHello7(){
 		User user = new User();
 		user.setRollNo(01);
@@ -72,7 +75,7 @@ public class HelloController {
 		return ResponseEntity.ok(user);	
 	}
 	
-	@RequestMapping(value = "hello8" , method = RequestMethod.POST)
+	@RequestMapping(value = "page7" , method = RequestMethod.POST)
 	public ResponseEntity<User> sayHello8(@RequestBody User user){
 		System.out.println(user);
 		user.setName(user.getName() + " Kumar");
@@ -80,15 +83,25 @@ public class HelloController {
 	}
 	
 //	@RestController
-	@RequestMapping(value = "hello9" , method = RequestMethod.POST)
+	@RequestMapping(value = "page8" , method = RequestMethod.POST)
 	public User sayHello9(@RequestBody User user){
-		user.setRollNo(22);
 		user.setName(user.getName() + " Kumar");
-		user.setAdderss("Chennai");
 		return user;
 	}
+	
+	@GetMapping(value = "page9")
+	public void sayHello10(@RequestParam int rollNo, String name, String adderss) {
+		System.out.println(rollNo);
+		System.out.println(name);
+		System.out.println(adderss);
+	}
+	
+	@GetMapping(value = "page10/{id}")
+	public void sayHello11(@PathVariable("id") int rollNo) {
+		System.out.println(rollNo);
+	}
 }
-
+	
 class User{
 	private int rollNo;
 	private String name;
